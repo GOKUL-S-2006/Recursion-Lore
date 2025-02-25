9ed6ba0f458e5f5e47884e20f2ba3c7a00a8c71f
GOKUL-S-2006 <gokul.sathiyamurthi2006@gmail.com>
2025-02-25 18:17:54 +0530

    Update Combination sum.cpp

--- Combination sum.cpp
+++ Combination sum.cpp
@@ -1,1 +1,34 @@
 
+class Solution {
+public:
+     void fc(int ind,int target,vector<int>&arr,vector<vector<int>>&ans,vector<int>&ds)
+     {
+        if(ind==arr.size())
+        {
+            if(target==0)
+            {
+                ans.push_back(ds);
+            }
+            return;
+        }
+            //pick condition
+            if(arr[ind]<=target)
+            {
+                ds.push_back(arr[ind]);
+                fc(ind,target-arr[ind],arr,ans,ds);
+                ds.pop_back();
+            }
+            //not pick
+            fc(ind+1,target,arr,ans,ds);
+        
+
+     }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        fc(0,target, candidates,ans,ds);
+        return ans;
+
+        
+    }
+};
